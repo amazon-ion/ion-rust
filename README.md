4d382b2e8e0d0bb8d561d95f955871e13f62f519
Almann Goo <almann.goo@gmail.com>
2020-08-03 13:20:22 -0700

    Adds Low-level Rust Bindings for Ion C (#35)
    * Adds ion-c submodule.
    * Adds build dependencies for `cmake` and `bindgen`.
    * Adds `build.rs`
      - Uses `cmake` crate to build and link Ion C.
      - Uses `bindgen` to create the raw bindings to Ion C.
    * Adds submodule documentation to `README.md`
    
    Co-authored-by: Therapon Skoteiniotis <therapon@amazon.com>

--- README.md
+++ README.md
@@ -4,4 +4,36 @@ A Rust implementation of the [Amazon Ion][spec] data format.
 
 ***This package is considered experimental, under active/early development, and the API is subject to change.***
 
-[spec]: http://amzn.github.io/ion-docs/docs/spec.html
+## Development
+
+This project uses a submodule to pull in [Ion C][ion-c] and [Ion Tests][ion-tests].  The easiest way to pull
+everything in is to clone the repository recursively:
+
+```bash
+$ git clone --recursive https://github.com/amzn/ion-rust
+```
+
+You can also initialize the submodules as follows:
+
+```bash
+$ git submodule update --init --recursive
+```
+
+Furthermore, you will need the [pre-requisties for `bindgen`][bindgen-req] installed which is basically
+`libclang`.
+
+Building the project:
+
+```bash
+$ cargo build --workspace --all-targets
+```
+
+Running all tests for `ion-rust` and `ion-c-sys`:
+
+```bash
+$ cargo test --workspace
+```
+
+[ion-c]: https://github.com/amzn/ion-c
+[ion-tests]: https://github.com/amzn/ion-tests
+[bindgen-req]: https://rust-lang.github.io/rust-bindgen/requirements.html
---
+++ ion-c-sys/build.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::path::PathBuf;
+
+/// Makes it easier to construct PathBufs instead of format!() which doesn't
+/// produce portable path separators.
+macro_rules! mkpath {
+    ( $($x:expr),* ) => {
+        {
+            let mut buf = PathBuf::new();
+            $(
+                buf.push($x);
+            )*
+            buf
+        }
+    };
+}
+
+fn main() {
+    let ionc_path = cmake::Config::new("ion-c").build();
+    let ionc_lib_path = mkpath!(&ionc_path, "lib");
+
+    println!("cargo:rustc-link-search=native={}", ionc_lib_path.display());
+    println!("cargo:rustc-link-lib=static=decNumber_static");
+    println!("cargo:rustc-link-lib=static=ionc_static");
+
+    let ionc_inc_path = mkpath!(&ionc_path, "include");
+    let ionc_main_header_path = mkpath!(&ionc_inc_path, "ionc", "ion.h");
+
+    let bindings = bindgen::Builder::default()
+        .header(ionc_main_header_path.to_str().unwrap())
+        // make sure we can find all the relevant headers
+        .clang_arg(format!("-I{}", ionc_inc_path.display()))
+        // defined in IonC's CMake configuration
+        .clang_arg("-DDECNUMDIGITS=34")
+        // invalidate the build whenever underlying headers change
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // emit the bindings
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("ionc_bindings.rs"))
+        .expect("Couldn't write bindings");
+}
