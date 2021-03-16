07e6d3505a1a3bf476edeacd14941dcec672057b
Almann Goo <almann@amazon.com>
2021-03-16 10:47:42 -0700

    Adds a preliminary `Element` DOM-like API.
    Provides an _borrowed_/_owned_ implementations of the traits in the
    `value` module.  The borrowed implementation is tied to a lifetime for
    string/binary data (but potentially more in the future).  The owned
    implementation is completely self contained.
    
    * Defines `SymbolToken` and `ImportSource` traits that is the basis for
      full symbol/field name/annotation support in `ion-rs`, but also is
      required for the `Element` trait.
    * APIs that return `Iterator` types currently use a `Box` to avoid the
      need for generic associated types (GAT).
    * Captures the variance of integers in Ion with the `IntAccess` trait
      and the `AnyInt` enum.
    * Currently implements the `Struct` trait via `Vec` of tuples, which
      will have to be refactored for O(N) access but is suitable for the
      iterator APIS.
    
    Starts to address #114, but is not nearly complete.  This commit is to
    provide the initial structure of this API for collaborative development,
    and to get early review.

---
+++ src/value/borrowed.rs
@@ -0,0 +1,229 @@
+// Copyright Amazon.com, Inc. or its affiliates.
+
+//! Provides borrowed implementations of [`SymbolToken`], [`Element`] and its dependents.
+//!
+//! Specifically, all implementations are tied to some particular lifetime, generally linked
+//! to a parser implementation of some sort or some context from which the borrow can occur.
+//! For simple values, the values are inlined (see [`BorrowedValue`]), but for things that are
+//! backed by octets or string data, `&[u8]` and `&str` are used.
+
+use super::{Element, ImportSource, Sequence, Struct, SymbolToken};
+use crate::types::SymbolId;
+use crate::value::AnyInt;
+use crate::IonType;
+
+/// A borrowed implementation of [`ImportSource`].
+#[derive(Debug, Copy, Clone)]
+pub struct BorrowedImportSource<'val> {
+    table: &'val str,
+    sid: SymbolId,
+}
+
+impl<'val> BorrowedImportSource<'val> {
+    pub fn new(table: &'val str, sid: SymbolId) -> Self {
+        Self { table, sid }
+    }
+}
+
+impl<'val> ImportSource for BorrowedImportSource<'val> {
+    fn table(&self) -> &str {
+        self.table
+    }
+
+    fn sid(&self) -> usize {
+        self.sid
+    }
+}
+
+/// A borrowed implementation of [`SymbolToken`].
+#[derive(Debug, Copy, Clone)]
+pub struct BorrowedSymbolToken<'val> {
+    text: Option<&'val str>,
+    local_sid: Option<SymbolId>,
+    source: Option<BorrowedImportSource<'val>>,
+}
+
+impl<'val> BorrowedSymbolToken<'val> {
+    pub fn new(
+        text: Option<&'val str>,
+        local_sid: Option<SymbolId>,
+        source: Option<BorrowedImportSource<'val>>,
+    ) -> Self {
+        Self {
+            text,
+            local_sid,
+            source,
+        }
+    }
+}
+
+impl<'val> From<&'val str> for BorrowedSymbolToken<'val> {
+    fn from(text: &'val str) -> Self {
+        Self::new(Some(text), None, None)
+    }
+}
+
+impl<'val> SymbolToken for BorrowedSymbolToken<'val> {
+    type ImportSource = BorrowedImportSource<'val>;
+
+    fn text(&self) -> Option<&str> {
+        self.text
+    }
+
+    fn local_sid(&self) -> Option<usize> {
+        self.local_sid
+    }
+
+    fn source(&self) -> Option<&Self::ImportSource> {
+        self.source.as_ref()
+    }
+}
+
+/// A borrowed implementation of [`Sequence`].
+#[derive(Debug, Clone)]
+pub struct BorrowedSequence<'val> {
+    children: Vec<BorrowedElement<'val>>,
+}
+
+impl<'val> BorrowedSequence<'val> {
+    pub fn new(children: Vec<BorrowedElement<'val>>) -> Self {
+        Self { children }
+    }
+}
+
+impl<'val> Sequence for BorrowedSequence<'val> {
+    type Element = BorrowedElement<'val>;
+
+    fn iter<'a>(&'a self) -> Box<dyn Iterator<Item = &'a Self::Element> + 'a> {
+        Box::new(self.children.iter())
+    }
+}
+
+/// A borrowed implementation of [`Struct`]
+#[derive(Debug, Clone)]
+pub struct BorrowedStruct<'val> {
+    // TODO model actual map indexing for the struct (maybe as a variant type)
+    //      otherwise struct field lookup will be O(N)
+    fields: Vec<(BorrowedSymbolToken<'val>, BorrowedElement<'val>)>,
+}
+
+impl<'val> BorrowedStruct<'val> {
+    pub fn new(fields: Vec<(BorrowedSymbolToken<'val>, BorrowedElement<'val>)>) -> Self {
+        Self { fields }
+    }
+}
+
+impl<'val> Struct for BorrowedStruct<'val> {
+    type FieldName = BorrowedSymbolToken<'val>;
+    type Element = BorrowedElement<'val>;
+
+    fn iter<'a>(
+        &'a self,
+    ) -> Box<dyn Iterator<Item = (&'a Self::FieldName, &'a Self::Element)> + 'a> {
+        // convert &(k, v) -> (&k, &v)
+        Box::new(self.fields.iter().map(|kv| match &kv {
+            (k, v) => (k, v),
+        }))
+    }
+}
+
+// TODO replace the references with `Cow` and bridge to the owned APIs for mutability
+
+/// Variants for all borrowed version _values_ within an [`Element`].
+#[derive(Debug, Clone)]
+pub enum BorrowedValue<'val> {
+    Null(IonType),
+    Integer(AnyInt),
+    String(&'val str),
+    Symbol(BorrowedSymbolToken<'val>),
+    SExpression(BorrowedSequence<'val>),
+    List(BorrowedSequence<'val>),
+    Struct(BorrowedStruct<'val>),
+    // TODO fill this in with the rest...
+}
+
+/// A borrowed implementation of [`Element`]
+#[derive(Debug, Clone)]
+pub struct BorrowedElement<'val> {
+    annotations: Vec<BorrowedSymbolToken<'val>>,
+    value: BorrowedValue<'val>,
+}
+
+impl<'val> BorrowedElement<'val> {
+    pub fn new(annotations: Vec<BorrowedSymbolToken<'val>>, value: BorrowedValue<'val>) -> Self {
+        Self { annotations, value }
+    }
+}
+
+impl<'val> From<BorrowedValue<'val>> for BorrowedElement<'val> {
+    /// Constructs a [`BorrowedElement`] without annotations from this value.
+    fn from(val: BorrowedValue<'val>) -> Self {
+        Self::new(vec![], val)
+    }
+}
+
+impl<'val> Element for BorrowedElement<'val> {
+    type SymbolToken = BorrowedSymbolToken<'val>;
+    type Sequence = BorrowedSequence<'val>;
+    type Struct = BorrowedStruct<'val>;
+
+    fn ion_type(&self) -> IonType {
+        use BorrowedValue::*;
+        match &self.value {
+            Null(t) => *t,
+            Integer(_) => IonType::Integer,
+            String(_) => IonType::String,
+            Symbol(_) => IonType::Symbol,
+            SExpression(_) => IonType::SExpression,
+            List(_) => IonType::List,
+            Struct(_) => IonType::Struct,
+        }
+    }
+
+    fn annotations<'a>(&'a self) -> Box<dyn Iterator<Item = &'a Self::SymbolToken> + 'a> {
+        Box::new(self.annotations.iter())
+    }
+
+    fn is_null(&self) -> bool {
+        match &self.value {
+            BorrowedValue::Null(_) => true,
+            _ => false,
+        }
+    }
+
+    fn as_any_int(&self) -> Option<&AnyInt> {
+        match &self.value {
+            BorrowedValue::Integer(i) => Some(i),
+            _ => None,
+        }
+    }
+
+    fn as_str(&self) -> Option<&str> {
+        match &self.value {
+            BorrowedValue::String(text) => Some(*text),
+            BorrowedValue::Symbol(sym) => sym.text(),
+            _ => None,
+        }
+    }
+
+    fn as_sym(&self) -> Option<&Self::SymbolToken> {
+        match &self.value {
+            BorrowedValue::Symbol(sym) => Some(sym),
+            _ => None,
+        }
+    }
+
+    fn as_sequence(&self) -> Option<&Self::Sequence> {
+        match &self.value {
+            BorrowedValue::SExpression(seq) | BorrowedValue::List(seq) => Some(seq),
+            _ => None,
+        }
+    }
+
+    fn as_struct(&self) -> Option<&Self::Struct> {
+        match &self.value {
+            BorrowedValue::Struct(structure) => Some(structure),
+            _ => None,
+        }
+    }
+}
---
+++ src/value/owned.rs
@@ -0,0 +1,228 @@
+// Copyright Amazon.com, Inc. or its affiliates.
+
+//! Provides owned implementations of [`SymbolToken`], [`Element`] and its dependents.
+//!
+//! This API is simpler to manage with respect to borrowing lifetimes, but requires full
+//! ownership of data to do so.
+
+use super::{AnyInt, Element, ImportSource, Sequence, Struct, SymbolToken};
+use crate::types::SymbolId;
+use crate::IonType;
+
+/// An owned implementation of  [`ImportSource`].
+#[derive(Debug, Clone)]
+pub struct OwnedImportSource {
+    table: String,
+    sid: SymbolId,
+}
+
+impl OwnedImportSource {
+    pub fn new<T: Into<String>>(table: T, sid: SymbolId) -> Self {
+        Self {
+            table: table.into(),
+            sid,
+        }
+    }
+}
+
+impl ImportSource for OwnedImportSource {
+    fn table(&self) -> &str {
+        &self.table
+    }
+
+    fn sid(&self) -> usize {
+        self.sid
+    }
+}
+
+/// An owned implementation of [`SymbolToken`].
+#[derive(Debug, Clone)]
+pub struct OwnedSymbolToken {
+    text: Option<String>,
+    local_sid: Option<SymbolId>,
+    source: Option<OwnedImportSource>,
+}
+
+impl OwnedSymbolToken {
+    pub fn new(
+        text: Option<String>,
+        local_sid: Option<SymbolId>,
+        source: Option<OwnedImportSource>,
+    ) -> Self {
+        Self {
+            text,
+            local_sid,
+            source,
+        }
+    }
+}
+
+impl<T: Into<String>> From<T> for OwnedSymbolToken {
+    /// Constructs an owned token that has only text.
+    fn from(text: T) -> Self {
+        Self::new(Some(text.into()), None, None)
+    }
+}
+
+impl SymbolToken for OwnedSymbolToken {
+    type ImportSource = OwnedImportSource;
+
+    fn text(&self) -> Option<&str> {
+        self.text.as_ref().map(String::as_str)
+    }
+
+    fn local_sid(&self) -> Option<usize> {
+        self.local_sid
+    }
+
+    fn source(&self) -> Option<&Self::ImportSource> {
+        self.source.as_ref()
+    }
+}
+
+/// An owned implementation of [`Sequence`]
+#[derive(Debug, Clone)]
+pub struct OwnedSequence {
+    children: Vec<OwnedElement>,
+}
+
+impl OwnedSequence {
+    pub fn new(children: Vec<OwnedElement>) -> Self {
+        Self { children }
+    }
+}
+
+impl Sequence for OwnedSequence {
+    type Element = OwnedElement;
+
+    fn iter<'a>(&'a self) -> Box<dyn Iterator<Item = &'a Self::Element> + 'a> {
+        Box::new(self.children.iter())
+    }
+}
+
+/// An owned implementation of [`Struct`]
+#[derive(Debug, Clone)]
+pub struct OwnedStruct {
+    // TODO model actual map indexing for the struct (maybe as a variant type)
+    //      otherwise struct field lookup will be O(N)
+    fields: Vec<(OwnedSymbolToken, OwnedElement)>,
+}
+
+impl OwnedStruct {
+    pub fn new(fields: Vec<(OwnedSymbolToken, OwnedElement)>) -> Self {
+        Self { fields }
+    }
+}
+
+impl Struct for OwnedStruct {
+    type FieldName = OwnedSymbolToken;
+    type Element = OwnedElement;
+
+    fn iter<'a>(
+        &'a self,
+    ) -> Box<dyn Iterator<Item = (&'a Self::FieldName, &'a Self::Element)> + 'a> {
+        // convert &(k, v) -> (&k, &v)
+        Box::new(self.fields.iter().map(|kv| match &kv {
+            (k, v) => (k, v),
+        }))
+    }
+}
+
+/// Variants for all owned version _values_ within an [`Element`].
+#[derive(Debug, Clone)]
+pub enum OwnedValue {
+    Null(IonType),
+    Integer(AnyInt),
+    String(String),
+    Symbol(OwnedSymbolToken),
+    SExpression(OwnedSequence),
+    List(OwnedSequence),
+    Struct(OwnedStruct),
+    // TODO fill this in with the rest of the value types...
+}
+
+/// An owned implementation of [`Element`]
+#[derive(Debug, Clone)]
+pub struct OwnedElement {
+    annotations: Vec<OwnedSymbolToken>,
+    value: OwnedValue,
+}
+
+impl OwnedElement {
+    pub fn new(annotations: Vec<OwnedSymbolToken>, value: OwnedValue) -> Self {
+        Self { annotations, value }
+    }
+}
+
+impl From<OwnedValue> for OwnedElement {
+    fn from(val: OwnedValue) -> Self {
+        Self::new(vec![], val)
+    }
+}
+
+impl Element for OwnedElement {
+    type SymbolToken = OwnedSymbolToken;
+    type Sequence = OwnedSequence;
+    type Struct = OwnedStruct;
+
+    fn ion_type(&self) -> IonType {
+        use OwnedValue::*;
+
+        match &self.value {
+            Null(t) => *t,
+            Integer(_) => IonType::Integer,
+            String(_) => IonType::String,
+            Symbol(_) => IonType::Symbol,
+            SExpression(_) => IonType::SExpression,
+            List(_) => IonType::List,
+            Struct(_) => IonType::Struct,
+        }
+    }
+
+    fn annotations<'a>(&'a self) -> Box<dyn Iterator<Item = &'a Self::SymbolToken> + 'a> {
+        Box::new(self.annotations.iter())
+    }
+
+    fn is_null(&self) -> bool {
+        match &self.value {
+            OwnedValue::Null(_) => true,
+            _ => false,
+        }
+    }
+
+    fn as_any_int(&self) -> Option<&AnyInt> {
+        match &self.value {
+            OwnedValue::Integer(i) => Some(i),
+            _ => None,
+        }
+    }
+
+    fn as_str(&self) -> Option<&str> {
+        match &self.value {
+            OwnedValue::String(text) => Some(text),
+            OwnedValue::Symbol(sym) => sym.text(),
+            _ => None,
+        }
+    }
+
+    fn as_sym(&self) -> Option<&Self::SymbolToken> {
+        match &self.value {
+            OwnedValue::Symbol(sym) => Some(sym),
+            _ => None,
+        }
+    }
+
+    fn as_sequence(&self) -> Option<&Self::Sequence> {
+        match &self.value {
+            OwnedValue::SExpression(seq) | OwnedValue::List(seq) => Some(seq),
+            _ => None,
+        }
+    }
+
+    fn as_struct(&self) -> Option<&Self::Struct> {
+        match &self.value {
+            OwnedValue::Struct(structure) => Some(structure),
+            _ => None,
+        }
+    }
+}
