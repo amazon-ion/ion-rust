3552d99cd8888674601a8efd870ce6e38a9b333b
Marc Bowes <bowes@amazon.com>
2021-05-14 10:22:16 -0700

    TestDigest now uses interior mutability
    The fixed-length output requirements of the Digest trait make
    integrating with the ion-hash-test suite a little annoying! Nothing
    wrong with the trait; rather it's a consequence of the design of the
    test suite..
    
    The tests define correct as "the underlying digest was fed a series of
    updates that look like this" - kind of like a mock/expect setup. Because
    different test cases have different sequences, this design clashes with
    the fixed length expectations of the Digest trait.
    
    To work around this, we completely ignore the fixed-length result in our
    tests and return assert on a result vec that lives outside of the Digest
    trait implementation.
    
    One wrinkle is that IonHasher wants to own the Digest so that it can be
    sure nobody else is writing to it. To work around that, we use interior
    mutability to allow our mock to be cloned and our tests keep a concrete
    reference to it.
    
    As a direct consequence of this change, I can enable the intLength512
    test which was longer that our previous 256 bit length limit. Also, the
    nonsense around trailing zeros is gone!

--- ion-hash/tests/ion_hash_tests.rs
+++ ion-hash/tests/ion_hash_tests.rs
@@ -1,11 +1,11 @@
 // Copyright Amazon.com, Inc. or its affiliates.
 
-use digest::{consts::U256, generic_array::GenericArray, Digest, Output};
+use digest::{consts::U8, generic_array::GenericArray, Digest, Output};
 use ion_hash::{self, IonHasher};
 use ion_rs::result::IonResult;
 use ion_rs::value::reader::{element_reader, ElementReader};
 use ion_rs::value::*;
-use std::fs::read;
+use std::{cell::RefCell, fs::read, rc::Rc};
 
 // This type exists purely to facilitate testing with ion-hash-test. See that
 // package for details on how the tests are structured.
@@ -15,32 +15,42 @@ use std::fs::read;
 // purpose is _not_ to test the hashing function! So.. `updates` track the byte
 // representations of values we incorporate into the hash.
 //
-// The `Digest` trait specifies a fixed length output. We pick a `N` that is
-// bigger than the largest "identity hash" test case. Note that this means tests
-// may have to deal with trailing zeros (e.g. the identity hash might be 56
-// bytes long, with 200 trailing zeros in the fixed-length array).
+// The `Digest` trait specifies a fixed length output. Because our tests aren't
+// really doing any hashing (they're just tracking the bytes sent to `update`),
+// we don't have a fixed length output! Instead, we have the tests look at a Vec
+// that tracks what was fed into `update`. This means that the result of calling
+// `finalize` is garbage.
+//
+// Because [`IonHasher`] wants to own the `Digest`, we use `Rc<RefCell<T>>` to
+// allow for cloning. This, in turn, allows our tests to retain access to the
+// mock. Why is this important? Because concrete access to the mock allows
+// access to the variable size update vec!
 #[derive(Default, Clone)]
 struct TestDigest {
-    updates: GenericArray<u8, U256>,
-    position: usize,
+    inner: Rc<RefCell<TestDigestInner>>,
+}
+
+#[derive(Default)]
+struct TestDigestInner {
+    updates: Vec<u8>,
 }
 
 impl Digest for TestDigest {
-    // Pick a number bigger than the biggest test case digest expectation.
-    type OutputSize = U256;
+    // This is not really used other than to satisfy the trait. See the comments
+    // on [`TestDigest`].
+    type OutputSize = U8;
 
     fn new() -> Self {
         Self {
-            updates: GenericArray::default(),
-            position: 0,
+            inner: Rc::new(RefCell::new(TestDigestInner {
+                updates: Vec::new(),
+            })),
         }
     }
 
     fn update(&mut self, bytes: impl AsRef<[u8]>) {
-        for b in bytes.as_ref() {
-            self.updates[self.position] = *b;
-            self.position += 1;
-        }
+        let mut inner = self.inner.borrow_mut();
+        inner.updates.extend(bytes.as_ref());
     }
 
     fn chain(self, _data: impl AsRef<[u8]>) -> Self
@@ -50,30 +60,41 @@ impl Digest for TestDigest {
         todo!()
     }
 
+    /// The output of this method should be ignored. This implementation **does
+    /// not support fixed size results**.
     fn finalize(self) -> Output<Self> {
-        self.updates
+        GenericArray::default()
     }
 
+    /// While this method does correctly reset the internal state, the result of
+    /// this method should be ignored for the same reason as noted in
+    /// [`finalize`].
     fn finalize_reset(&mut self) -> Output<Self> {
-        let output = self.updates;
-        self.updates = GenericArray::default();
-        self.position = 0;
-        output
+        let mut inner = self.inner.borrow_mut();
+        inner.updates.clear();
+        GenericArray::default()
     }
 
     fn reset(&mut self) {
-        self.updates = GenericArray::default();
-        self.position = 0;
+        self.finalize_reset();
     }
 
     fn output_size() -> usize {
-        256
+        8
     }
 
+    /// The output of this method should be ignored (see `[finalize]`).
     fn digest(data: &[u8]) -> Output<Self> {
         let mut myself = Self::new();
         myself.update(data);
         myself.finalize()
+    }
+}
+
+impl TestDigest {
+    fn inspect_updates(&self) -> Vec<u8> {
+        let inner = self.inner.borrow();
+        inner.updates.clone()
     }
 }
 
@@ -113,7 +134,12 @@ fn test_case<E: Element>(ion: &E, strukt: &E) -> IonResult<()> {
     let digest = TestDigest::default();
     let hasher = IonHasher::new(digest.clone());
     let test_case_name = test_case_name(ion)?;
-    let result = hasher.hash_element(ion)?;
+
+    // We completely ignore the result here! See [`TestDigest`] for why!
+    let _ = hasher.hash_element(ion)?;
+    // .. instead, we use the result that's waiting in our mock digest
+    // implementation.
+    let result = digest.inspect_updates();
 
     for it in identity.iter() {
         let method = it
@@ -137,17 +163,7 @@ fn test_case<E: Element>(ion: &E, strukt: &E) -> IonResult<()> {
                 // diagnose bugs.
             }
             "digest" => {
-                // Because `TestDigest` uses generic array (fixed size,
-                // intialized with zeros) and isn't doing real hashing, we land
-                // up an array that has our result filled in to N bytes and then
-                // a bunch of trailing zeros. We want to ignore those, since
-                // they're not relevant.
-                let ignore_trailing_zeros = result
-                    .iter()
-                    .rposition(|b| *b != 0)
-                    .map(|p| p + 1)
-                    .unwrap_or(bytes.len());
-                let result_slice = &result[0..ignore_trailing_zeros];
+                let result_slice = &result[..];
 
                 // Convert into hex repr to make assertion failures look like
                 // the test case definitions.
