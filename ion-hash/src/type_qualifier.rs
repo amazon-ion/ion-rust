6f4def135586c66f690f8608b7c0061fa26db520
Marc Bowes <bowes@amazon.com>
2021-05-27 19:19:38 -0700

    Implement IonHash support for structs
    Quite a lot going on in this commit! Structs are different from the
    scalars we've been hashing so far in that they create **new hashers**:
    
    > escape(concat(sort(H(field1), H(field2), ..., H(fieldn))))
    
    The big `H` there is an actual hash computation.
    
    This immediately means that `IonHasher` not be a useful abstraction
    since the requirement to be able to make new hashers means that asking
    for the initial hasher is a little silly. So, in this commit I stripped
    that struct away. This creates a bit of weirdness in lib.rs where there
    are a bunch of functions that take a mutable hasher as their second
    argument. My intention is to add some other abstraction (maybe a trait)
    in a future commit. [fn:1]
    
    The next bump in the road was that all our abstractions coupled
    "dispatch" and "implementation", where the former means "look at the Ion
    Type then do something specific for that type". This posed a problem
    when computing the field hash: the key (of a field) is a Symbol and a
    Symbol is *not an Element*. To fix this, I decoupled dispatch and
    implementation, allowing the field hash computation to go directly to
    the TQ/repr infrastructure for symbols.
    
    The biggest bump by *far* was yet more trouble with the ion-hash-test
    suite. Using a real hasher (sha256) became extremely tricky with the
    field hashers because the expectations don't show that. For example:
    
    ```ion
    {
      ion:{'':1},              // empty symbol
        // ^^^^ NOTE: this field requires a *new hasher*
      expect:{ identity:(
        update::(0x0b) update::(0xd0)
        // --------LOOK DOWN! -------------------
            update::(0x0b) update::(0x70) update::(0x0e)
            update::(0x0b) update::(0x20) update::(0x01) update::(0x0e)
            digest::(0x0b 0x70 0x0e 0x0b 0x20 0x01 0x0e)
        // ^ this idented sequence is on a new instance of the hasher
        // --------LOOK UP! ---------------------
          update::(0x0c 0x0b 0x70 0x0c 0x0e 0x0c 0x0b 0x20 0x01 0x0c 0x0e)
        update::(0x0e)
        digest::(0x0b 0xd0 0x0c 0x0b 0x70 0x0c 0x0e 0x0c 0x0b 0x20 0x01 0x0c 0x0e 0x0e)
      ) }
    }
    ```
    
    So, I've gone back to an `IdentityHasher` with fixed size output and
    worked around the trailing zeros.
    
    A few (3) of the struct tests fail because structs also test other types
    that we haven't implemented. I added an ignore list so that we can have
    all the struct tests in this file and clearly show which aren't yet
    expected to pass.
    
    [fn:1]: The struct representation has some duplicate sequence code that
    I plan to fix at the same time.

--- ion-hash/src/type_qualifier.rs
+++ ion-hash/src/type_qualifier.rs
@@ -7,10 +7,15 @@ use std::slice;
 ///! [spec]: https://amzn.github.io/ion-hash/docs/spec.html.
 use ion_rs::{
     binary::IonTypeCode,
-    value::{AnyInt, Element},
+    value::{AnyInt, Element, Sequence, Struct, SymbolToken},
     IonType,
 };
 use num_bigint::Sign;
+
+// For many types, the qualifier is either 'null' or 'not null'. That's what
+// these constants are for!
+const QUALIFIER_NOT_NULL: u8 = 0x00;
+const QUALIFIER_NULL: u8 = 0x0F;
 
 pub(crate) struct TypeQualifier(u8);
 
@@ -30,55 +35,27 @@ impl TypeQualifier {
     /// laid out in the spec. In many cases, the `T` is determined by the Ion
     /// binary type code.
     ///
-    /// As much as possible, we use `const fn` to implement lookups logically.
-    /// The code is authored to read like the spec, without sacrificing
-    /// performance.
+    /// This method simply calls the appropriate `type_qualifier_$ion_type`
+    /// method.
     pub(crate) fn from_element<E>(elem: &E) -> TypeQualifier
     where
         E: Element + ?Sized,
     {
-        use IonTypeCode::*;
         match elem.ion_type() {
-            IonType::Null => combine(NullOrWhitespace, 0x0F),
-            IonType::Boolean => {
-                let q = match elem.as_bool() {
-                    None => 0x0F,
-                    Some(b) => match b {
-                        false => 0x00,
-                        true => 0x01,
-                    },
-                };
-                combine(Boolean, q)
-            }
-            IonType::Integer => {
-                let any = elem.as_any_int();
-                let t = match is_anyint_positive(any) {
-                    true => IonTypeCode::PositiveInteger,
-                    false => IonTypeCode::NegativeInteger,
-                };
-                let q = match any {
-                    Some(_) => 0x00,
-                    None => 0x0F,
-                };
-                combine(t, q)
-            }
-            IonType::Float => combine(Float, 0x00),
+            IonType::Null => type_qualifier_null(),
+            IonType::Boolean => type_qualifier_boolean(elem.as_bool()),
+            IonType::Integer => type_qualifier_integer(elem.as_any_int()),
+            IonType::Float => type_qualifier_float(elem.as_f64()),
             /*IonType::Decimal => Decimal,
-            IonType::Timestamp => Timestamp,
-            IonType::Symbol => Symbol,*/
-            IonType::String => {
-                let q = match elem.as_str() {
-                    Some(_) => 0x00,
-                    None => 0x0F,
-                };
-                combine(String, q)
-            }
-            /*IonType::Clob => Clob,
-            IonType::Blob => Blob,
-            IonType::List => List,
-            IonType::SExpression => SExpression,*/
-            /*IonType::Struct => combine(Struct, 0),*/
-            _ => todo!(),
+            IonType::Timestamp => Timestamp,*/
+            IonType::Symbol => type_qualifier_symbol(elem.as_sym()),
+            IonType::String => type_qualifier_string(elem.as_str()),
+            IonType::Clob => type_qualifier_clob(elem.as_bytes()),
+            IonType::Blob => type_qualifier_blob(elem.as_bytes()),
+            IonType::List => type_qualifier_list(elem.as_sequence()),
+            IonType::SExpression => type_qualifier_sexp(elem.as_sequence()),
+            IonType::Struct => type_qualifier_struct(elem.as_struct()),
+            other => unimplemented!("TypeQualifier is not implemented for {}", other),
         }
     }
 
@@ -96,4 +73,80 @@ fn is_anyint_positive(value: Option<&AnyInt>) -> bool {
             AnyInt::BigInt(b) => !std::matches!(b.sign(), Sign::Minus),
         },
     }
+}
+
+/// For many types, the qualifier (see: [`TypeQualifier`]) is based on whether
+/// the value is/not null.
+fn qualify_nullness<T>(value: Option<T>) -> u8 {
+    match value {
+        Some(_) => QUALIFIER_NOT_NULL,
+        None => QUALIFIER_NULL,
+    }
+}
+
+pub(crate) fn type_qualifier_null() -> TypeQualifier {
+    combine(IonTypeCode::NullOrWhitespace, 0x0F)
+}
+
+pub(crate) fn type_qualifier_boolean(value: Option<bool>) -> TypeQualifier {
+    let q = match value {
+        None => 0x0F,
+        Some(b) => match b {
+            false => 0x00,
+            true => 0x01,
+        },
+    };
+    combine(IonTypeCode::Boolean, q)
+}
+
+pub(crate) fn type_qualifier_integer(any: Option<&AnyInt>) -> TypeQualifier {
+    let t = match is_anyint_positive(any) {
+        true => IonTypeCode::PositiveInteger,
+        false => IonTypeCode::NegativeInteger,
+    };
+    combine(t, qualify_nullness(any))
+}
+
+pub(crate) fn type_qualifier_float(value: Option<f64>) -> TypeQualifier {
+    combine(IonTypeCode::Float, qualify_nullness(value))
+}
+
+pub(crate) fn type_qualifier_string(value: Option<&str>) -> TypeQualifier {
+    combine(IonTypeCode::String, qualify_nullness(value))
+}
+
+pub(crate) fn type_qualifier_clob(value: Option<&[u8]>) -> TypeQualifier {
+    combine(IonTypeCode::Clob, qualify_nullness(value))
+}
+
+pub(crate) fn type_qualifier_blob(value: Option<&[u8]>) -> TypeQualifier {
+    combine(IonTypeCode::Blob, qualify_nullness(value))
+}
+
+pub(crate) fn type_qualifier_list<S>(value: Option<&S>) -> TypeQualifier
+where
+    S: Sequence + ?Sized,
+{
+    combine(IonTypeCode::List, qualify_nullness(value))
+}
+
+pub(crate) fn type_qualifier_sexp<S>(value: Option<&S>) -> TypeQualifier
+where
+    S: Sequence + ?Sized,
+{
+    combine(IonTypeCode::SExpression, qualify_nullness(value))
+}
+
+pub(crate) fn type_qualifier_symbol<S>(sym: Option<&S>) -> TypeQualifier
+where
+    S: SymbolToken + ?Sized,
+{
+    combine(IonTypeCode::Symbol, qualify_nullness(sym))
+}
+
+pub(crate) fn type_qualifier_struct<S>(value: Option<&S>) -> TypeQualifier
+where
+    S: Struct + ?Sized,
+{
+    combine(IonTypeCode::Struct, qualify_nullness(value))
 }
