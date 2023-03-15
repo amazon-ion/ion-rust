2fbde84f1379dd1fd15ddee7df1acb7d76ae2a97
Zack Slayton <zslayton@amazon.com>
2023-03-15 17:27:07 -0400

    one more link to GitHub issue 484

--- src/binary/mod.rs
+++ src/binary/mod.rs
@@ -3,7 +3,9 @@
 //! This module provides the necessary structures and logic to read values from a binary Ion
 //! data stream.
 
+// Public as a workaround for: https://github.com/amazon-ion/ion-rust/issues/484
 pub mod binary_writer;
+
 pub mod constants;
 pub mod decimal;
 mod header;
