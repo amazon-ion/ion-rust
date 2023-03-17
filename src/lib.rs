1fc8c6a0b65b9a759c249ff0308dac0688cb7d65
Zack Slayton <zslayton@amazon.com>
2023-03-17 19:54:59 -0400

    Adds a basic README

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,164 @@
 #![allow(dead_code)]
 #![deny(rustdoc::broken_intra_doc_links)]
+//! # Reading and writing `Element`s
+//!
+//! The [Element] API offers a convenient way to read and write Ion data when its exact shape is
+//! not known ahead of time.
+//!
+//! Each `Element` represents an `(annotations, value)` pair. If the value is a container (an Ion
+//! `list`, `sexp`, or `struct`), then it will contain its own collection of `Element`s. `Element`s
+//! can be nested to arbitrary depth.
+//!
+//! ## Constructing an `Element`
+//!
+//! ### From text Ion
+//! The [Element::read_one] method will parse the provided data and requires that it contain exactly
+//! one Ion value.
+//! ```
+//! # use ion_rs::IonResult;
+//! # fn main() -> IonResult<()> {
+//! use ion_rs::element::owned::Element;
+//! use ion_rs::IonType;
+//! let ion_data = "[1, 2, 3]";
+//! let element = Element::read_one(ion_data)?;
+//! assert_eq!(element.ion_type(), IonType::List);
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! [Element::read_all] will read any number of Ion values and return them as a `Vec<Element>`.
+//!
+//! [Element::read_first] will read the first Ion value without requiring that the stream have
+//! exactly one value.
+//!
+//! ### From a Rust value
+//! Most Rust primitives implement `Into<Element>`, allowing them to be converted to an Ion [Element]
+//! directly.
+//! ```
+//! # use ion_rs::IonResult;
+//! # fn main() -> IonResult<()> {
+//! use ion_rs::element::owned::Element;
+//!
+//! let int: Element = 5.into();
+//! assert_eq!(Element::read_one("5")?, int);
+//!
+//! let boolean: Element = true.into();
+//! assert_eq!(Element::read_one("true")?, boolean);
+//!
+//! let string: Element = "hello".into();
+//! assert_eq!(Element::read_one("\"hello\"")?, string);
+//!
+//! let ion_version_marker: &[u8] = &[0xE0, 0x01, 0x00, 0xEA]; // Ion 1.0 version marker
+//! let blob: Element = ion_version_marker.into();
+//! assert_eq!(Element::read_one("{{4AEA6g==}}")?, blob);
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! ### Using macros
+//!
+//! When constructing a container [Element], you can use the [`ion_list!`], [`ion_sexp!`],
+//! and [`ion_struct!`] macros.
+//!
+//! ```
+//! # use ion_rs::IonResult;
+//! # fn main() -> IonResult<()> {
+//! use ion_rs::element::owned::Element;
+//! use ion_rs::{ion_list, ion_sexp, ion_struct};
+//!
+//! // Variable names are allowed
+//! let six = 6i64;
+//! let list: Element = ion_list! [true, six, "foo"].into();
+//! assert_eq!(Element::read_one("[true, 6, \"foo\"]")?, list);
+//!
+//! // Nested use of macros is allowed
+//! // Notice that ion_sexp! uses ()s without commas
+//! let sexp: Element = ion_sexp! (true six ion_list!["foo", "bar"]).into();
+//! assert_eq!(Element::read_one("(true 6 [\"foo\", \"bar\"])")?, sexp);
+//!
+//! let field_name = "bar";
+//! let struct_: Element = ion_struct! {
+//!   "foo": six,
+//!   field_name: false
+//! }.into();
+//! assert_eq!(Element::read_one("{foo: 6, bar: false}")?, struct_);
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! ### From a stream
+//!
+//! ```no_run
+//! # use ion_rs::IonResult;
+//! # fn main() -> IonResult<()> {
+//! use std::fs::File;
+//! use ion_rs::element::owned::Element;
+//! use ion_rs::element::reader::ElementReader;
+//! use ion_rs::ReaderBuilder;
+//! let ion_file = File::open("/foo/bar/baz.ion").unwrap();
+//! let mut reader = ReaderBuilder::default().build(ion_file)?;
+//! // A simple pretty-printer
+//! for element in reader.elements() {
+//!     println!("{}", element?)
+//! }
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! ## Traversing an Element
+//!
+//! ```
+//! # use ion_rs::IonResult;
+//! # fn main() -> IonResult<()> {
+//! use ion_rs::element::owned::{Element, IntoAnnotatedElement, IonSequence, Value};
+//! use ion_rs::{ion_struct, ion_list};
+//! let element: Element = ion_struct! {
+//!   "foo": "hello",
+//!   "bar": true,
+//!   "baz": ion_list! [4, 5, 6]
+//! }.into();
+//!
+//! if let Value::Struct(s) = element.value() {
+//!   if let Some(Value::List(l)) = s.get("baz").map(|b| b.value()) {
+//!     for (index, element) in l.elements().enumerate() {
+//!         println!("{}. {}", index+1, element);
+//!         // 1) 4
+//!         // 2) 5
+//!         // 3) 6
+//!     }
+//!   }
+//! }
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! ## Writing to an `io::Write`
+//!
+//! ```
+//! # use ion_rs::IonResult;
+//! # fn main() -> IonResult<()> {
+//! use ion_rs::element::owned::{Element, IntoAnnotatedElement, IonSequence, Value};
+//! use ion_rs::{ion_struct, ion_list, TextWriterBuilder, IonWriter};
+//! use ion_rs::element::writer::ElementWriter;
+//! let element: Element = ion_struct! {
+//!   "foo": "hello",
+//!   "bar": true,
+//!   "baz": ion_list! [4, 5, 6]
+//! }.into();
+//!
+//! let mut buffer: Vec<u8> = Vec::new();
+//! let mut writer = TextWriterBuilder::default().build(&mut buffer)?;
+//! writer.write_element(&element)?;
+//! writer.flush()?;
+//! assert_eq!(
+//!   "{foo: \"hello\", bar: true, baz: [4, 5, 6]}".as_bytes(),
+//!   writer.output().as_slice()
+//! );
+//! # Ok(())
+//! # }
+//! ```
 
+use element::owned::Element;
 pub mod result;
 
 pub mod binary;
@@ -29,8 +187,11 @@ mod symbol_table;
 mod system_reader;
 mod writer;
 
+#[doc(inline)]
 pub use data_source::IonDataSource;
+#[doc(inline)]
 pub use raw_symbol_token::RawSymbolToken;
+#[doc(inline)]
 pub use raw_symbol_token_ref::RawSymbolTokenRef;
 
 pub use symbol::Symbol;
