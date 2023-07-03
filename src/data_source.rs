50bee50dab90f5c6cc8cb8baa16cad6614d15de0
Zack Slayton <zslayton@amazon.com>
2023-07-03 10:01:15 -0400

    Switches doc comment to usual format.

--- src/data_source.rs
+++ src/data_source.rs
@@ -261,10 +261,8 @@ mod tests {
     }
 }
 
-#[doc = r##"
-Types that implement this trait can be converted into an implementation of [BufRead], allowing
-users to deserialize Ion from a variety of types that might not define I/O operations on their own.
-"##]
+/// Types that implement this trait can be converted into an implementation of [BufRead], allowing
+/// users to deserialize Ion from a variety of types that might not define I/O operations on their own.
 pub trait ToIonDataSource {
     type DataSource: IonDataSource;
     fn to_ion_data_source(self) -> Self::DataSource;
