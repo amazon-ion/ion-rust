00e41a19d421508a91d2003dc1e68469f91155d3
Zack Slayton <zslayton@amazon.com>
2023-07-12 17:21:47 -0400

    Error types now hold a Cow<str> instead of requiring a String

--- src/result/illegal_operation.rs
+++ src/result/illegal_operation.rs
@@ -1,19 +1,20 @@
+use std::borrow::Cow;
 use thiserror::Error;
 
 #[derive(Clone, Debug, Error, PartialEq)]
 #[error("the user has performed an operation that is not legal in the current state: {operation}")]
 pub struct IllegalOperation {
-    operation: String,
+    operation: Cow<'static, str>,
 }
 
 impl IllegalOperation {
-    pub(crate) fn new(description: impl Into<String>) -> Self {
+    pub(crate) fn new(description: impl Into<Cow<'static, str>>) -> Self {
         IllegalOperation {
             operation: description.into(),
         }
     }
 
     pub fn operation(&self) -> &str {
-        self.operation.as_str()
+        self.operation.as_ref()
     }
 }
